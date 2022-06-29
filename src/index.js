ab6543b03ca6a8450a221f788d8cb46a9c84ea10
Andrgoit <stakgrom@gmail.com>
2022-06-29 10:07:42 +0300

    add card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,100 @@
+import ApiService from './js/api-service';
+const newApiService = new ApiService();
+
+//  == refs ==
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('#gallery-list'),
+  galleryItem: document.querySelector('.gallery-item'),
 };
 
+//   == listeners ==
 refs.form.addEventListener('submit', inputHandler);
 
-function inputHandler(e) {
+//  == First Load Page ==
+firstPageLoader();
+
+function firstPageLoader() {
+  window.addEventListener('DOMContentLoaded', getTrendingMovie);
+}
+
+async function getTrendingMovie() {
+  newApiService.resetPage();
+  try {
+    const response = await newApiService.fetchTrendingUrl();
+    const responseGenres = await newApiService.fetchGenres();
+    console.log(response);
+    markupMovies(response, responseGenres);
+  } catch (error) {
+    console.log(error);
+  }
+}
+//  == // First Load Page ==
+
+function markupMovies(response, responseGenres) {
+  console.log(response.results);
+  const arrGenres = responseGenres.genres;
+  console.log(arrGenres);
+
+  const imageUrl = 'https://image.tmdb.org/t/p/';
+  const imageSize = 'w500';
+  const movies = response.results;
+
+  const markup = movies
+    .map(
+      ({
+        id,
+        genre_ids,
+        original_title,
+        release_date,
+        poster_path,
+        vote_average,
+      }) => {
+        let genresArray = arrGenres
+          .filter(genre => genre_ids.includes(genre.id))
+          .map(genre => genre.name);
+        let date = new Date(release_date);
+        let year = date.getFullYear();
+        let vote = vote_average.toFixed(1);
+        return `<li class="gallery-item" id="${id}">
+        <div class="gallery-image"><img src="${imageUrl}${imageSize}${poster_path}" alt="${original_title}"></div>
+        <h2 class="gallery-item-title">${original_title}</h2>
+        <span class="gallery-item-prop">${genresArray} | ${year} | ${vote}</span>
+        <button type="button" id="watched">Watched</button>
+<button type="button" id="queue">Queue</button>
+      </li>`;
+      }
+    )
+    .join('');
+
+  refs.gallery.innerHTML = markup;
+  addGalleryItemListener();
+}
+
+async function inputHandler(e) {
   e.preventDefault();
-  const input = e.currentTarget.elements.searchInput.value;
-  console.log(input);
+  refs.gallery.innerHTML = '';
+  newApiService.input = e.currentTarget.elements.searchInput.value.trim();
+  // console.log(newApiService.input);
+  // newApiService.resetPage();
+  if (newApiService.input !== '') {
+    try {
+      const response = await newApiService.fetchSearch();
+      const responseGenres = await newApiService.fetchGenres();
+      // console.log(response);
+      markupMovies(response, responseGenres);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return;
+}
+
+function addGalleryItemListener() {
+  galleryItem = document.querySelector('.gallery-item');
+  galleryItem.addEventListener('click', galleryItemListener);
+}
+
+function galleryItemListener(e) {
+  console.log(e);
 }
---
+++ src/js/api-service.js
@@ -0,0 +1,58 @@
+const API_KEY = '83cba2c85d0df477852b094af9fbdddb';
+
+export default class ApiService {
+  constructor() {
+    this.searchValue = '';
+    this.page = 1;
+  }
+
+  async fetchTrendingUrl() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${this.page}`
+    );
+
+    this.incrementPage();
+
+    return await response.json();
+  }
+
+  async fetchSearch() {
+    // this.resetPage();
+    console.log(this.searchValue);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${this.searchValue}&page=${this.page}`
+    );
+    console.log();
+    this.incrementPage();
+    console.log(response);
+    return await response.json();
+  }
+
+  async fetchGenres() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+    );
+    // console.log('fetchGenres', response);
+    return await response.json();
+  }
+  // id подставлен статически временно
+  async fetchMovieById() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/157336?api_key=${API_KEY}`
+    );
+    return await response.json();
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get input() {
+    return this.searchValue;
+  }
+  set input(newSearchValue) {
+    this.searchValue = newSearchValue;
+  }
+}
