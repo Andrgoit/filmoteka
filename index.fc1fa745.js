9c450b39c9ad415375a5c94e6354535570285463
Andrgoit <93829515+Andrgoit@users.noreply.github.com>
2022-06-29 07:08:24 +0000

    Deploying to gh-pages from @ Andrgoit/filmoteka@ab6543b03ca6a8450a221f788d8cb46a9c84ea10 🚀

--- index.fc1fa745.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".search-form"),gallery:document.querySelector("#gallery-list")}).form.addEventListener("submit",(function(e){e.preventDefault();const r=e.currentTarget.elements.searchInput.value;console.log(r)}));
-//# sourceMappingURL=index.fc1fa745.js.map
